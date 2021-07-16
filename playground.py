4c2ff29a59dc73daf00bca40b742aa072fcf4e11
highergear <ndrew.sebastian@gmail.com>
2021-07-16 18:45:01 +0700

    just update

--- playground.py
+++ playground.py
@@ -8,15 +8,16 @@ game = [[0, 0, 0],
         [0, 0, 0]]
 
 
-def game_board(player=0, row=0, column=0, just_display=False):
+def game_board(game_map=game, player=0, row=0, column=0, just_display=False):
     print('   a  b  c')
     if not just_display:
-        global game  # to make the change of the game object applied globally
-        game[row][column] = player
+        # global game_map  # to make the change of the game object applied globally
+        game_map[row][column] = player
 
-    for count, row in enumerate(game):
+    for count, row in enumerate(game_map):
         print(count, row)
+    return game_map
 
 
-game_board(just_display=True)
-game_board(1, 2, 2)
+game_map = game_board(just_display=True)
+game_map = game_board(game, 1, 2, 2)
