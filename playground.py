3e119ca6092e1966c20cde1863115cdb1c083b4a
highergear <ndrew.sebastian@gmail.com>
2021-07-16 18:17:58 +0700

    initial commit

--- playground.py
+++ playground.py
@@ -2,14 +2,20 @@ def adding(i1: int, i2: int) -> int:
     return i1 + i2
 
 
-def game_board():
-    game = [[0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0]]
+game = [[0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]]
 
+
+def game_board(player=0, row=0, column=0, just_display=False):
     print('   a  b  c')
+
+    if not just_display:
+        game[row][column] = player
+
     for count, row in enumerate(game):
         print(count, row)
 
 
-x = game_board()
+game_board(just_display=True)
+game_board(1,2,2)
